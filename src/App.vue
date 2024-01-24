abddfe354365de84a7a4b14f03a2674fa0b638cd
martaqh <marta.k.herma@gmail.com>
2024-01-24 23:05:28 +0000

    Add basic structure of TheButton component

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,21 @@ import TheButton from "@/components/TheButton.vue";
 </script>
 
 <template>
-  <TheButton text="Hey" />
+  <main class="app">
+    <TheButton href="http://www.risify.pl" color="dark-green"> </TheButton>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app {
+  padding: 24px;
+  margin: auto;
+  position: absolute;
+  inset: 0px;
+  width: 70vw;
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/components/TheButton.vue
+++ src/components/TheButton.vue
@@ -1,11 +1,78 @@
 <script setup lang="ts">
-defineProps<{
-  text?: string;
-}>();
+import { computed } from "vue";
+
+type Size = "small" | "regular" | "large";
+type Variant = "filled" | "outlined";
+type Color = "orange" | "green" | "dark-green" | "red" | "yellow";
+
+interface Props {
+  to?: string;
+  href?: string;
+  size: Size;
+  variant: Variant;
+  color: Color;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: "regular",
+  variant: "filled",
+  color: "yellow",
+});
+
+const buttonType = computed(() => {
+  if (props.to) {
+    return "router-link";
+  } else if (props.href) {
+    return "a";
+  } else {
+    return "button";
+  }
+});
+
+const buttonSize = computed(() => {
+  if (props.to) {
+    return "router-link";
+  } else if (props.href) {
+    return "a";
+  } else {
+    return "button";
+  }
+});
+
+const textColor = computed(() => {
+  return props.color === "yellow" ? "dark-green" : "white";
+});
 </script>
 
 <template>
-  <component>{{ text }} </component>
+  <component class="button" :is="buttonType" :href="href" :to="to">
+    <slot class="slot"> Click me </slot>
+  </component>
 </template>
 
-<style scoped></style>
+<style scoped>
+.button {
+  padding: 24px 14px;
+  height: fit-content;
+  border-radius: 24px;
+  background-color: v-bind("`var(--${color})`");
+  color: v-bind("`var(--${textColor})`");
+  font-size: 16px;
+  font-weight: 600;
+  font-family: Montserrat;
+  line-height: 125%;
+  letter-spacing: -0.48px;
+  border: none;
+
+  &:hover {
+    background-color: v-bind("`var(--${color}-hover`");
+  }
+  &:focus {
+    border: 2px solid v-bind("`var(--${color}-hover`");
+  }
+}
+
+.slot {
+  display: flex;
+}
+</style>
